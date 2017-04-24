b6c3fa3340ce5d3e9239e36a6fc7216de399ffa6
cpgeier <cpg3rb@virginia.edu>
2017-04-24 14:35:01 -0400

    Adding Scene

---
+++ MuseumProject/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            Renderer r = GetComponent<Renderer>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+            if (movie.isPlaying)
+            {
+                movie.Pause();
+            }
+            else
+            {
+                movie.Play();
+            }
+        }
+    }
+}
