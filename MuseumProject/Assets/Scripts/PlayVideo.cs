753f572ff5903b2b746b9850a4dabfe8cdb47b34
Julian <jaykmcclinton@gmail.com>
2017-04-26 12:04:36 -0400

    Video Players

--- MuseumProject/Assets/Scripts/PlayVideo.cs
+++ MuseumProject/Assets/Scripts/PlayVideo.cs
@@ -5,23 +5,33 @@ using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour {
 
+    bool inRange;
+
     public VideoPlayer vid;
     public AudioSource aud;
+
 	// Use this for initialization
 	void Start () {
-        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Interact")) {
-            if (aud.isPlaying) {
-                aud.Pause();
+        if (Input.GetButtonDown("Interact") && inRange) {
+            if (vid.isPlaying) {
+                if (aud != null) aud.Pause();
                 vid.Pause();
             } else {
-                aud.Play();
+                if (aud != null) aud.Play();
                 vid.Play();
             }
         }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        inRange = true;
+    }
+
+    void OnTriggerExit(Collider col) {
+        inRange = false;
     }
 }
