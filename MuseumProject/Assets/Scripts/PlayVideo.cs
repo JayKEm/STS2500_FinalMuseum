1697cb1d975e6fc63606f7a5325f8622198d1822
Julian <jaykmcclinton@gmail.com>
2017-04-26 11:37:10 -0400

    Compiling Main Museum
    Finally it's coming together

--- MuseumProject/Assets/Scripts/PlayVideo.cs
+++ MuseumProject/Assets/Scripts/PlayVideo.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour {
 
+    public VideoPlayer vid;
+    public AudioSource aud;
 	// Use this for initialization
 	void Start () {
         //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
@@ -11,6 +14,14 @@ public class PlayVideo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (Input.GetButtonDown("Interact")) {
+            if (aud.isPlaying) {
+                aud.Pause();
+                vid.Pause();
+            } else {
+                aud.Play();
+                vid.Play();
+            }
+        }
     }
 }
